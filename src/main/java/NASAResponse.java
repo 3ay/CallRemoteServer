3fa7d5e4dc0eebbc5c697979c78e40bf796ca2eb
3ay <lefrost.volkoff@yandex.ru>
2023-05-12 12:53:41 +0300

    refactor variable's names

--- src/main/java/NASAResponse.java
+++ src/main/java/NASAResponse.java
@@ -10,9 +10,9 @@ public class NASAResponse {
     @JsonProperty("hdurl")
     public String hdurl;
     @JsonProperty("media_type")
-    public String media_type;
+    public String mediaType;
     @JsonProperty("service_version")
-    public String service_version;
+    public String serviceVersion;
     @JsonProperty("title")
     public String title;
     @JsonProperty("url")
@@ -32,8 +32,8 @@ public class NASAResponse {
                 "\n date: " + date +
                 "\n explanation: " + explanation +
                 "\n hdurl: " + hdurl +
-                "\n media_type: " + media_type +
-                "\n service_version: " + service_version +
+                "\n media_type: " + mediaType +
+                "\n service_version: " + serviceVersion +
                 "\n title: " + title +
                 "\n url: " + url;
     }
